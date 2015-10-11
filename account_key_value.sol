c93d025157d8d2d538b691390a1cfe6ac57b02e5
Jonathan Brown <jbrown@bluedroplet.com>
2015-10-11 15:37:37 +0700

    Simplify contract.

--- account_key_value.sol
+++ account_key_value.sol
@@ -4,27 +4,22 @@
  */
 contract AccountKeyValue {
 
-  mapping (address => mapping (bytes32 => bytes)) values;
+  mapping (address => mapping (string => bytes)) values;
 
-  // TODO: add c4 after https://github.com/ethereum/solidity/issues/120
-  event Update(string indexed c1, string indexed c2, string indexed c3) anonymous;
+  event logUpdate(address indexed account, string key) anonymous;
 
-  function keyHash(string c1, string c2, string c3, string c4, string c5) internal returns (bytes32) {
-    return sha3(c1, '.', c2, '.', c3, '.', c4, '.', c5);
+  function valueGet(address account, string key) constant returns (bytes) {
+    return values[account][key];
   }
 
-  function getValue(address account, string c1, string c2, string c3, string c4, string c5) constant returns (bytes) {
-    return values[account][keyHash(c1, c2, c3, c4, c5)];
+  function valueSet(string key, bytes value) {
+    values[msg.sender][key] = value;
+    logUpdate(msg.sender, key);
   }
 
-  function setValue(bytes value, string c1, string c2, string c3, string c4, string c5) {
-    values[msg.sender][keyHash(c1, c2, c3, c4, c5)] = value;
-    Update(c1, c2, c3);
-  }
-
-  function deleteValue(string c1, string c2, string c3, string c4, string c5) {
-    delete values[msg.sender][keyHash(c1, c2, c3, c4, c5)];
-    Update(c1, c2, c3);
+  function valueDelete(string key) {
+    delete values[msg.sender][key];
+    logUpdate(msg.sender, key);
   }
 
 }
